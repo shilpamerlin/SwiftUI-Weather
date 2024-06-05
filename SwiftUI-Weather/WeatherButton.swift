f000f01aaedb71bff89eb8189d932aabb43d923f
Shilpa Merlin joy <shilpamjoy@gmail.com>
2024-06-05 13:14:24 -0400

    UI Done

--- SwiftUI-Weather/WeatherButton.swift
+++ SwiftUI-Weather/WeatherButton.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundcolor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundcolor)
+            .foregroundStyle(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
